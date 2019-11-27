eb5d853874bfc4456a0a65f0bc67cfabc527a7ab
Ahmadh1 <Ahmadh_1@mail.com>
2019-11-27 19:55:35 +0500

    Primary Setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import './App.css'
+import logo from './logo.svg'
+import Survey from './Components/Survey';
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} alt="React" className="App-logo" />
+          Firebase React Survey App
+        </div>
+        <Survey />
+      </div>
+    );
+  }
 }
-
-export default App;
